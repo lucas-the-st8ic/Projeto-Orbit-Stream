52117366f46ae58fbccd3970825a9f589c7bf9f5
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-02-13 17:35:18 -0300

    updates

--- OrbitStream/src/main/java/br/com/alura/OrbitStream/OrbitStreamApplication.java
+++ OrbitStream/src/main/java/br/com/alura/OrbitStream/OrbitStreamApplication.java
@@ -2,12 +2,16 @@ package br.com.alura.OrbitStream;
 
 import br.com.alura.OrbitStream.model.DadosEpisodio;
 import br.com.alura.OrbitStream.model.DadosSerie;
+import br.com.alura.OrbitStream.model.DadosTemporada;
 import br.com.alura.OrbitStream.service.ConsumoApi;
 import br.com.alura.OrbitStream.service.ConverterDados;
 import br.com.alura.OrbitStream.service.IConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class OrbitStreamApplication implements CommandLineRunner {
@@ -27,23 +31,42 @@ public class OrbitStreamApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         var consumoApi = new ConsumoApi();
-
-        /*var json = consumoApi.buscarDados("https://www.omdbapi.com/?t=Sons+of+Anarchy&apikey=32905f12");
-        System.out.println(json);*/
-
         ConverterDados conversor = new ConverterDados();
-        
-/*        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 
-        System.out.println(dados);*/
-
-        var json = consumoApi.buscarDados("https://www.omdbapi.com/?t=Sons+of+" +
-                "Anarchy&Season=1&Episode=1&apikey=32905f12");
+        //DADOS DA SÉRIE
+        var json = consumoApi.buscarDados("https://www.omdbapi.com/?t=Sons+of+Anarchy&apikey=32905f12");
         System.out.println(json);
-        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+
+        System.out.println(dados);
+        // FIM DADOS DA SÉRIE
+
+
+        //DADOS DE EPISÓDIOS
+       /*
+       var json = consumoApi.buscarDados("https://www.omdbapi.com/?t=Sons+of+Anarchy&Season=1&Episode=2&apikey=32905f12");
+
+        System.out.println(json);
+
+        DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 
         System.out.println(dadosEpisodio);
 
+
+        System.out.println(json);*/
+        // FIM DADOS DE EPISÓDIOS
+
+        List <DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            var jsonTemporadas = consumoApi.buscarDados("https://www.omdbapi.com/?t=Sons+of+Anarchy&Season=" + i + "&apikey=32905f12");
+
+            DadosTemporada dadosTemporada = conversor.obterDados(jsonTemporadas, DadosTemporada.class);
+
+            temporadas.add(dadosTemporada);
+        }
+
+        temporadas.forEach(System.out::println);
     }
 }
---
+++ OrbitStream/src/main/java/br/com/alura/OrbitStream/model/DadosTemporada.java
@@ -0,0 +1,17 @@
+package br.com.alura.OrbitStream.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporada( @JsonAlias("Season") Integer numeroTemporada,
+                              @JsonAlias("Episodes") List<DadosEpisodio> listaDeEpisodio) {
+
+    @Override
+    public String toString() {
+        return "Temporada: " + numeroTemporada +
+                "Episódios: " + listaDeEpisodio;
+    }
+}
