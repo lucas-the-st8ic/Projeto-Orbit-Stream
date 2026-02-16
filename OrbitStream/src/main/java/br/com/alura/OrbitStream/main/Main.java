7062df4041798ace7facf37892df2d537dd323a4
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-02-16 12:45:36 -0300

    Update Main.java

--- OrbitStream/src/main/java/br/com/alura/OrbitStream/main/Main.java
+++ OrbitStream/src/main/java/br/com/alura/OrbitStream/main/Main.java
@@ -1,5 +1,6 @@
 package br.com.alura.OrbitStream.main;
 
+import br.com.alura.OrbitStream.model.DadosEpisodio;
 import br.com.alura.OrbitStream.model.DadosSerie;
 import br.com.alura.OrbitStream.model.DadosTemporada;
 import br.com.alura.OrbitStream.service.ConsumoApi;
@@ -37,6 +38,16 @@ public class Main {
             temporadas.add(dadosTemporada);
         }
 
-        temporadas.forEach(System.out::println);
+        /*temporadas.forEach(System.out::println);*/
+
+ /*       for(int i = 0; i < dados.totalTemporadas(); i++){
+            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).listaDeEpisodio();
+
+            for (int j = 0; j < episodiosTemporada.size(); j++) {
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }*/
+
+        temporadas.forEach(t -> t.listaDeEpisodio().forEach(e -> System.out.println(e.titulo())));
     }
 }
