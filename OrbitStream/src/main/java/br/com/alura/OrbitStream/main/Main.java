337faf8184153a61dfa078adf514d6064631fb1b
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-04-24 16:38:23 -0300

    updates

--- OrbitStream/src/main/java/br/com/alura/OrbitStream/main/Main.java
+++ OrbitStream/src/main/java/br/com/alura/OrbitStream/main/Main.java
@@ -99,12 +99,7 @@ public class Main {
 
     private void listarSeriesBuscadas() {
 
-        List<Serie> series = new ArrayList<>();
-
-        series = dadosSeries.stream()
-                        .map(d -> new Serie(d))
-                                .collect(Collectors.toList());
-
+        List<Serie> series = repositorio.findAll();
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenero))
                 .forEach(System.out::println);
