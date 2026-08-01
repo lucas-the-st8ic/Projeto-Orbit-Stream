29dd993b6cb26d180b5f14b6911599b8ddd5a281
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-01 19:49:47 -0300

    Update OrbitStreamApplication.java

--- OrbitStream/src/main/java/br/com/alura/OrbitStream/OrbitStreamApplication.java
+++ OrbitStream/src/main/java/br/com/alura/OrbitStream/OrbitStreamApplication.java
@@ -1,9 +1,5 @@
 package br.com.alura.OrbitStream;
 
-import br.com.alura.OrbitStream.main.Main;
-import br.com.alura.OrbitStream.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
