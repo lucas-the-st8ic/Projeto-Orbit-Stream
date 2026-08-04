b32c40524d68b27ad0d81d129b7e70b490b62756
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-04 14:06:34 -0300

    Update SerieController.java

--- OrbitStream/src/main/java/br/com/alura/OrbitStream/controller/SerieController.java
+++ OrbitStream/src/main/java/br/com/alura/OrbitStream/controller/SerieController.java
@@ -1,13 +1,21 @@
 package br.com.alura.OrbitStream.controller;
 
+import br.com.alura.OrbitStream.model.Serie;
+import br.com.alura.OrbitStream.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository serieRepository;
+
     @GetMapping("/series")
-    public String obterSeries() {
-        return "Aqui vão ser listadas as séries";
+    public List<Serie> obterSeries() {
+        return serieRepository.findAll();
     }
 }
