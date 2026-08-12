fdd618daaae13f5f0cd664efe1ddc0273c21335e
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-12 13:32:20 -0300

    Update SerieController.java

--- OrbitStream/src/main/java/br/com/alura/OrbitStream/controller/SerieController.java
+++ OrbitStream/src/main/java/br/com/alura/OrbitStream/controller/SerieController.java
@@ -50,14 +50,11 @@ public class SerieController {
         return service.obterTemporadasPorNumero(id, numero);
     }
 
-    @GetMapping("/categorias/{nomeGenero}")
+    @GetMapping("/categoria/{nomeGenero}")
     public List<SerieDTO> obterSeriesPorCategoria(@PathVariable String nomeGenero) {
         return service.obterSeriesPorCategoria(nomeGenero);
     }
-
-
-
-
+    
     @GetMapping("/traduzir-sinopses")
     public ResponseEntity<String> traduzirSinopses() {
         try {
