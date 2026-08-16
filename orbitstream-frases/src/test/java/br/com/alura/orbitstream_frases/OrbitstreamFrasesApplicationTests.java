ed48f211ed436986034b49967ef27cb7bc425be5
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-16 17:49:44 -0300

    Updates

---
+++ orbitstream-frases/src/test/java/br/com/alura/orbitstream_frases/OrbitstreamFrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.orbitstream_frases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbitstreamFrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
