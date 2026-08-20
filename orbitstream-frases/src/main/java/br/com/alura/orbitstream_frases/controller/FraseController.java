790014473256c7e5e9406b1f256e7dc92a555fee
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-20 17:32:02 -0300

    Update FraseController.java

--- orbitstream-frases/src/main/java/br/com/alura/orbitstream_frases/controller/FraseController.java
+++ orbitstream-frases/src/main/java/br/com/alura/orbitstream_frases/controller/FraseController.java
@@ -7,13 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/series")
+@RestController
 public class FraseController {
 
     @Autowired
     private FraseService fraseService;
 
-    @GetMapping("/frases")
+    @GetMapping("/series/frases")
     public FraseDTO obterFraseAleatoria() {
         return fraseService.obterFraseAleatoria();
     }
