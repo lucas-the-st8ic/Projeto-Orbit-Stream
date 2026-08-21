0cf618e7a85db1171e52c484249cce601d0748c3
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-08-21 15:55:33 -0300

    Create CorsConfiguration.java

---
+++ orbitstream-frases/src/main/java/br/com/alura/orbitstream_frases/configuration/CorsConfiguration.java
@@ -0,0 +1,7 @@
+package br.com.alura.orbitstream_frases.configuration;
+
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration implements WebMvcConfigurer {
+    
+}
